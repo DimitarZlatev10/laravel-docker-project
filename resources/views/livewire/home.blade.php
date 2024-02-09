e839a396f5663a3616f2468b6143b757a9a72d9d
dimitarzlatev <dimitarzlatev02@gmail.com>
2024-02-09 22:50:57 +0200

    installed breeze and livewire

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <p> mitkokurabatep</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class)->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
